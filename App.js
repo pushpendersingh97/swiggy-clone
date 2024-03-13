a9bcc2f808e73cfef5f192e796dbecec8544c0eb
Pushpender Singh <36353150+pushpendersingh97@users.noreply.github.com>
2024-03-13 18:50:34 +0530

    iniial commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import ReactDOM from 'react-dom/client';
+
+import React from 'react'
+
+export const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />)
