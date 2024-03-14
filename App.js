f1496342deb2f490727e519b95699f1f8d8c3518
Pushpender Singh <pushpendersingh311@gmail.com>
2024-03-14 19:30:54 +0530

    CArd and header added

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import ReactDOM from 'react-dom/client';
 
 import React from 'react'
+import { Header } from './components/Header';
+import { Card } from './components/Card';
 
 export const App = () => {
   return (
-    <div>App</div>
+    <div className='app'>
+      <Header />
+      <Card name="Pushpender" time="30-40 mins" cusines="North Indian, Biryani" area="Sector 47" />
+    </div>
   )
 }
 
