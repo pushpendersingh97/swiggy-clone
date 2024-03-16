4f9d3f64537fa65f9be53c55dc450a010af534fc
Pushpender Singh <pushpendersingh311@gmail.com>
2024-03-16 21:33:14 +0530

    Commented Infinite scroll

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,35 +1,50 @@
 import React, { useEffect, useState } from "react";
 import { RestaurantCard } from "./RestaurantCard";
-import { SWIGGY_URL } from "../utils/constant";
+import { SWIGGY_GET_URL } from "../utils/constant";
 import { Shimmer } from "../common/shimmer";
 
 export const Body = () => {
   let [listOfRestaurant, setListOfRestraunt] = useState([]);
+  let [isFetching, setIsFetching] = useState(true);
+
+  const getRestaurants = async () => {
+    const response = await fetch(SWIGGY_GET_URL);
+    const restaurant = await response.json();
+    setListOfRestraunt(
+      restaurant?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants
+    );
+  };
+
+  // const handleScroll = () => {
+  //   if (window.innerHeight + document.documentElement.scrollTop !== document.documentElement.offsetHeight || isFetching) {
+  //     return;
+  //   }
+
+  //   setIsFetching(true);
+  //   getMoreRestaurants();
+  // };
+
+  // function getMoreRestaurants() {
+  //   console.log("Scrolling work");
+  //   setIsFetching(false);
+  // }
+
+  // useEffect(() => {
+  //   window.addEventListener("scroll", handleScroll);
+  //   return () => window.removeEventListener("scroll", handleScroll);
+  // }, [isFetching]);
 
   useEffect(() => {
-    fetch(SWIGGY_URL)
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        console.log(
-          data?.data.cards[4]?.card?.card?.gridElements?.infoWithStyle
-            ?.restaurants
-        );
-        setListOfRestraunt(
-          data?.data.cards[4]?.card?.card?.gridElements?.infoWithStyle
-            ?.restaurants
-        );
-      })
-      .catch((err) => {
-        console.error(err);
-      });
+    getRestaurants();
   }, []);
 
-  if (listOfRestaurant && listOfRestaurant.length === 0) {
-    return <div className="container">
-      <Shimmer />
-    </div>;
+  if (listOfRestaurant && listOfRestaurant.length === 0 && isFetching) {
+    return (
+      <div className="container">
+        <Shimmer />
+      </div>
+    );
   }
 
   return (
@@ -37,6 +52,8 @@ export const Body = () => {
       {listOfRestaurant.map((res) => {
         return <RestaurantCard resData={res.info} key={res.info.id} />;
       })}
+
+      {isFetching ?? <Shimmer />}
     </div>
   );
 };
--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,2 +1,3 @@
-export const IMG_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"
-export const SWIGGY_URL = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.45970&lng=77.02820&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+export const IMG_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+export const SWIGGY_GET_URL = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.45970&lng=77.02820&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+export const SWIGGY_UPDATE_URL = "https://www.swiggy.com/dapi/restaurants/list/update";
