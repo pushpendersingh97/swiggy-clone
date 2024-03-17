2205c2d29b15c169cc17f9ef66f3918e83d0db8f
Pushpender Singh <pushpendersingh311@gmail.com>
2024-03-17 22:59:57 +0530

    Fixing Body Component:

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -8,25 +8,21 @@ import { useRestaurantList } from "../utils/custom-hooks/useRestaurantList";
 export const Body = () => {
   const listOfRestaurant = useRestaurantList();
 
-  if (listOfRestaurant === null) {
-    return (
-      <div className="container mx-auto">
-        <Shimmer />
-      </div>
-    );
-  }
-
   return (
     <div className="container mx-auto">
       <div className="bg-white">
         <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-          <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-            {listOfRestaurant.map((res) => (
-              <Link to={res.info.id} key={res.info.id} className="group">
-                <RestaurantCard resData={res.info} />
-              </Link>
-            ))}
-          </div>
+          {listOfRestaurant === null ? (
+            <Shimmer />
+          ) : (
+            <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+              {listOfRestaurant.map((res) => (
+                <Link to={res.info.id} key={res.info.id} className="group">
+                  <RestaurantCard resData={res.info} />
+                </Link>
+              ))}
+            </div>
+          )}
         </div>
       </div>
     </div>
