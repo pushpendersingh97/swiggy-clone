8b8358238b023308f7327ed05d8c58be271d7dca
Pushpender Singh <pushpendersingh311@gmail.com>
2024-03-17 18:47:39 +0530

    Added Page not Found Compnent and routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import ReactDOM from 'react-dom/client';
-
 import React from 'react'
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
 import { Header } from './components/Header';
 import { Body } from './components/Body';
+import { About } from './components/About';
+import { PageNotFound } from './common/PageNotFound';
 
 export const App = () => {
   return (
@@ -13,6 +16,17 @@ export const App = () => {
   )
 }
 
+const appRouter = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <PageNotFound />
+  },
+  {
+    path: "/about",
+    element: <About />
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />)
+root.render(<RouterProvider router={appRouter} />)
